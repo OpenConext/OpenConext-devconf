5dcb1e47493b760d687f2cfac84867dc8ce7eb36
Bart Geesink <bart.geesink@surf.nl>
2023-06-15 16:21:45 +0200

    Middleware: Add provisioning scripts for middleware

---
+++ stepup/middleware/middleware-push-whitelist.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CWD=$(pwd)
+
+function error_exit {
+	echo "${1}"
+	if [ -n "${TMP_FILE}" -a -d "${TMP_FILE}" ]; then
+		rm "${TMP_FILE}"
+	fi
+	cd ${CWD}
+	exit 1
+}
+
+# Script to write the middleware config
+
+TMP_FILE=$(mktemp -t midcfg.XXXXXX)
+if [ $? -ne "0" ]; then
+	error_exit "Could not create temp file"
+fi
+
+echo "Pushing new institution whitelist to: http://middleware.dev.openconext.local/management/whitelist/replace"
+
+http_response=$(curl -k --write-out %{http_code} --output ${TMP_FILE} -XPOST -s \
+	-u management:secret -H "Accept: application/json" \
+	-H "Content-type: application/json" \
+	-d @middleware-whitelist.json \
+	https://middleware.dev.openconext.local/management/whitelist/replace)
+
+output=$(cat ${TMP_FILE})
+rm ${TMP_FILE}
+echo $output
+
+res=$?
+if [ $res -ne "0" ]; then
+	error_exit "Curl failed with code $res"
+fi
+
+# Check for HTTP 200
+if [ "${http_response}" -ne "200" ]; then
+	error_exit "Unexpected HTTP response: ${http_response}"
+fi
+
+# On success JSON output should start with: {"status":"OK"
+ok_count=$(echo "${output}" | grep -c "status")
+if [ $ok_count -ne "1" ]; then
+	error_exit "Expected one JSON \"status: OK\" in response, found $ok_count"
+fi
+
+echo "OK. New config pushed"
