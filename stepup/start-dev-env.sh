f735caa0f39fc889cadd522819e7925864e050eb
Dan <dan@hostatic.ro>
2023-07-05 16:02:09 +0300

    Make sure we can pass extra parameters to the script
    Also print some extra debug messages

--- stepup/start-dev-env.sh
+++ stepup/start-dev-env.sh
@@ -5,4 +5,6 @@ export SERVICE=$1
 export CODE_PATH=$2
 
 # Use docker compose to start the environment but with the modified override file
-docker-compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up
+echo -e "Starting the dev environment with the following command:\n"
+echo -e "docker-compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"\n"
+docker-compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"
