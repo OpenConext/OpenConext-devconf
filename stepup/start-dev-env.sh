0dd90e742e420d1af0935b5ed36dee5aafe38862
Michiel Kodde <mkodde@ibuildings.nl>
2023-08-15 09:46:41 +0200

    Use docker compose not docker-compose

--- stepup/start-dev-env.sh
+++ stepup/start-dev-env.sh
@@ -6,5 +6,5 @@ export CODE_PATH=$2
 
 # Use docker compose to start the environment but with the modified override file
 echo -e "Starting the dev environment with the following command:\n"
-echo -e "docker-compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"\n"
-docker-compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"
+echo -e "docker compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"\n"
+docker compose -f docker-compose.yml -f ./${SERVICE}/docker-compose.override.yml up "${@:3}"
