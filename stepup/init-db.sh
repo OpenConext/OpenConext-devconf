f15fc55023b4c963d17c60e80f686960ef4f0698
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2023-09-21 17:22:09 +0200

    Fix symfony cache ownership after running console command as root

--- stepup/init-db.sh
+++ stepup/init-db.sh
@@ -7,9 +7,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Initializing middleware and gateway databases"
 docker compose exec middleware /var/www/html/bin/console  doctrine:migrations:migrate --env=prod --em=deploy --no-interaction
+docker compose exec middleware chown -R www-data /var/www/html/var/cache
 
 echo "Initializing webauthn database"
 docker compose exec webauthn /var/www/html/bin/console doctrine:migrations:migrate --env=prod --no-interaction
+docker compose exec webauthn chown -R www-data /var/www/html/var/cache
 
 echo "Pushing middleware configuration"
 sh "${DIR}/middleware/middleware-push-config.sh"
